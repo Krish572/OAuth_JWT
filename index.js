e078efed290b40c9621e835a1e49db89c06fee83
Leelakrishna Y <sushant05072@gmail.com>
2025-12-26 09:59:37 +0530

    retest OAuth

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require('mongoose');
 const passport = require("passport");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
 const app = express();
-require("./passport.js")
+require("./passport.js");
+
 app.use(passport.initialize());
 
+app.get("/home", function(req, res){
+    res.status(200).json({message: "You are in home"});
+})
 
 app.get("/login", function(req, res){
-    res.send('<a href="/auth/google">login with google</a>');
+    res.send('<a href="http://localhost:3000/auth/google">login with google</a>');
 })
 
-app.get("/auth/google", 
-    passport.authenticate("google", {scope: ["profile", "email"], prompt: "consent"})
-);
+app.get("/auth/google", passport.authenticate("google", {scope : ["profile", "email"], prompt: "consent"}))
 
-app.get("/api/v1/google/callback", passport.authenticate("google", {session: false}),
- async function(req, res) {
-    console.log(req.user);
-    const token = await jwt.sign({userId: req.user.id}, process.env.JWT_SECRET);
-    return res.status(200).json({token});
+app.get("/api/v1/google/callback", passport.authenticate("google", {session: false}), (req, res) => {
+    const token = jwt.sign({userId : req.user._id}, process.env.JWT_SECRET);
+    return res.status(200).json(token);
 })
-
 
 function startServer(){
     mongoose.connect(process.env.MONGOOSE_URL)
     .then(() => {
+        console.log("DB Connected Succesfully");
         app.listen(process.env.PORT, function(){
-            console.log("Server is listening on Port " + process.env.PORT)
+            console.log("Server is listening on Port " + process.env.PORT);
         })
     })
     .catch(err => {
-        console.log("DB connection failed");
+        console.log("DB Connection failed");
     })
 }
 
 startServer();
+
+
 
 
 
--- passport.js
+++ passport.js
@@ -5,31 +5,29 @@ const User = require("./user.model.js");
 passport.use(
     new GoogleStrategy(
         {
-            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientID : process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY,
             callbackURL: "http://localhost:3000/api/v1/google/callback"
-        }, 
-        async (userToken, refreshToken, profile, done) => {
-                try{
-                    console.log(profile);
+        },
+        async (__, _, profile, done) => {
+            try{
                     const email = profile.emails[0].value;
-                    let userFound = await User.findOne({
-                        $or : [{email}, {"authProviders.googleId": profile.id}]
-                    });
-                    if(!userFound){
-                        userFound = await User.create({
-                            email,
-                            authProviders: { googleId : profile.id}
-                        })
-                    }else if(!userFound.authProviders?.googleId){
-                        userFound.authProviders.googleId = profile.id;
-                        await userFound.save();
-                    }
-                    done(null, userFound);
+                userFound = await User.findOne({
+                    $or: [{email} , {authProviders : {googleId : profile.id}}]
+                });
+                if(!userFound){
+                    userFound = await User.create({
+                        email,
+                        authProviders: {googleId : profile.id}
+                    })
+                }else if(!userFound.authProviders?.googleId){
+                    userFound.authProviders.googleId = profile.id;
+                    await userFound.save();
                 }
-                catch(err){
-                    done(err, null);
-                }
+                done(null, userFound);
+            }catch(err){
+                done(err, null);
+            }
         }
     )
 )
